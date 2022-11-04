02f2361ab60c932631384048085814fe4b856cc4
ginjardev <arthurshur@gmail.com>
2022-11-04 20:00:46 +0000

    alarm plays

--- alarm_clock_gui.py
+++ alarm_clock_gui.py
@@ -26,22 +26,23 @@ def alarm(set_alarm_timer):
         #winsound.PlaySound("sound.wav", winsound.SND_ASYNC)
         global tkSnack
         snd = tkSnack.Sound()
-        snd.read('sound.wav')
+        snd.read('dripchord.wav')
         snd.play(blocking=1)
         break
 
 
 def actual_time():
-    set_alarm_timer = f"{hour.hour}:{hour.minute}:{hour.second}"
+    set_alarm_timer = f"{hour.get()}:{min.get()}:{sec.get()}"
     alarm(set_alarm_timer)
 
 
 #clock gui 
 
 clock.title("Alarm Clock")
-clock.geometry('400x200')
+clock.geometry('400x300')
 time_format = Label(clock, text="Enter time in 24 hour format!", fg="green", bg="black", font="Arial").place(x=60, y=120)
 add_time = Label(clock, text="Hour Min Sec", font=60).place(x=110)
+Label(clock, text='').pack()
 set_your_alarm = Label(clock, text="When to wake you up", fg="blue", relief = "solid",font=("Helevetica",7,"bold")).place(x=0, y=29)
 
 
@@ -50,6 +51,11 @@ min = StringVar()
 sec = StringVar()
 
 
+hourTime = Entry(clock, textvariable= hour, bg="pink", width=15).place(x=110, y=30)
+minTime = Entry(clock, textvariable=min, bg="pink", width=15).place(x=150, y=30)
+secTime = Entry(clock, textvariable=sec, bg="pink", width=15).place(x=200, y=30)
+
+submit = Button(clock, text="Set Alarm", fg="red", width=10, command=actual_time).place(x=110, y=70)
 
 
 clock.mainloop()
