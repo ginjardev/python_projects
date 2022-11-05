4e5b8bd73be3b494f5ca0e60d75a2f4d06f65c5f
ginjardev <arthurshur@gmail.com>
2022-11-05 01:26:20 +0000

    binary search iterative

--- binary_search.py
+++ binary_search.py
@@ -34,3 +34,39 @@ if position == -1:
     print("Element not in list")
 else: 
     print("Element is at position: " + str(position))
+
+
+# iterative approach
+
+length = int(input("Enter length of list: "))
+int_list = []
+
+for i in range(length):
+    element = int(input("Enter an element: "))
+    int_list.append(element)
+
+int_list = sorted(int_list)
+print(int_list)
+target = int(input("Enter target element: "))
+
+#loop for binary search
+
+start = 0
+end = length - 1
+position = -1
+
+while(start <=end):
+    mid = (start + end ) // 2
+    if int_list[mid] == target:
+        position = mid
+        break
+    elif target < int_list[mid]:
+        end = mid - 1
+    elif target > int_list[mid]:
+        start = mid + 1
+
+
+if position == -1:
+    print("Element not in list")
+else: 
+    print("Element is at position: " + str(position))
