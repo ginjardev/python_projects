14b7e3252856826dc14ec3b4846396e8adaad460
ginjardev <arthurshur@gmail.com>
2022-11-16 00:53:53 +0000

    update gui

--- contact_book_gui.py
+++ contact_book_gui.py
@@ -23,16 +23,16 @@ rf_bg = "Gray35"
 frame_font = ("Garamond", 14)
 
 # StringVar variables
-name = StringVar()
-phone = StringVar()
-email = StringVar()
-search = StringVar()
+name_str = StringVar()
+phone_str = StringVar()
+email_str = StringVar()
+search_str = StringVar()
 
 # the functions
 def submit_record(): 
-    global name, email, phone, address_entry
+    global name_str, email_str, phone_str, address_entry
     global cursor
-    name, email, phone, address = name.get(), email.get(), phone.get(), address_entry.get(1.0, END)
+    name, email, phone, address = name_str.get(), email_str.get(), phone_str.get(), address_entry.get(1.0, END)
 
     if name == '' or email == '' or phone == '' or address == '':
         messagebox.showerror('Error!', 'Please fill all the fields')
@@ -41,7 +41,7 @@ def submit_record():
             "INSERT INTO CONTACT_BOOK (NAME, EMAIL, PHONE_NUMBER, ADDRESS) VALUES (?, ?, ?, ?)", (name, email, phone, address)
         )
         connector.commit()
-        messagebox.showinfo('Contact added, contact stored successfully')
+        messagebox.showinfo('Contact added', 'Contact added')
         listbox.delete(0, END)
         list_contacts()
         clear_fields()
@@ -61,7 +61,7 @@ def delete_record():
     cursor.execute('DELETE FROM CONTACT_BOOK WHERE NAME= ?', (listbox.get(ACTIVE)))
     connector.commit()
 
-    messagebox.showinfo("Contact Deleted")
+    messagebox.showinfo("Contact Deleted", "Contact Deleted")
     listbox.delete(0,END)
     list_contacts()
 
@@ -70,38 +70,38 @@ def delete_all_records():
     cursor.execute('DELETE FROM CONTACT_BOOK')
     connector.commit()
 
-    messagebox.showinfo("All records deleted")
+    messagebox.showinfo("All records deleted", "All records deleted")
     listbox.delete(0, END)
     list_contacts()
 
 
 def view_record():
-    global name, phone, email, address_entry, listbox
+    global name_str, phone_str, email_str, address_entry, listbox
 
     curr = cursor.execute(
-        "SELCT FROM CONTACT_BOOK WHERE NAME = ?", listbox.get(ACTIVE)
+        "SELECT * FROM CONTACT_BOOK WHERE NAME = ?", listbox.get(ACTIVE)
     )
     values = curr.fetchall()[0]
 
-    name.set(values[1]); phone.set(values[3]); email.set(values[2])
+    name_str.set(values[1]); phone_str.set(values[3]); email_str.set(values[2])
 
     address_entry.delete(1.0, END)
     address_entry.insert(END, values[4])
 
 
 def clear_fields():
-    global name, phone, email, address_entry, listbox
+    global name_str, phone_str, email_str, address_entry, listbox
 
     listbox.select_clear(0, END)
 
-    name.set("")
-    phone.set("")
-    email.set("")
+    name_str.set('')
+    phone_str.set('')
+    email_str.set('')
     address_entry.delete(1.0, END)
 
 
 def search_record():
-    query = str(search.get())
+    query = str(search_str.get())
 
     if query != '':
         listbox.delete(0, END)
@@ -134,18 +134,18 @@ right_frame.place(relx=0.6, relwidth=0.4, relheight=1, y=30)
 # components left frame
 Label(left_frame, text='Name', bg=lf_bg, font=frame_font).place(relx=0.3, rely=0.05)
 
-name_entry = Entry(left_frame, width=15, font=("Verdana", 11), textvariable=name)
+name_entry = Entry(left_frame, width=15, font=("Verdana", 11), textvariable=name_str)
 name_entry.place(relx=0.1, rely=0.1)
 
 
 Label(left_frame, text='Phone No.', bg=lf_bg, font=frame_font).place(relx=0.23, rely=0.2)
 
-phone_entry = Entry(left_frame, width=15, font=("Verdana", 11), textvariable=phone)
+phone_entry = Entry(left_frame, width=15, font=("Verdana", 11), textvariable=phone_str)
 phone_entry.place(relx=0.1, rely=0.25)
 
 Label(left_frame, text="Email", bg=lf_bg, font=frame_font).place(relx=0.3, rely=0.35)
 
-email_entry = Entry(left_frame, width=15, font=("Verdana", 11), textvariable=email)
+email_entry = Entry(left_frame, width=15, font=("Verdana", 11), textvariable=email_str)
 email_entry.place(relx=0.1, rely=0.4)
 
 Label(left_frame, text="Address", bg=lf_bg, font=frame_font).place(relx=0.28, rely=0.5)
@@ -155,7 +155,7 @@ address_entry.place(relx=0.1, rely=0.55)
 
 # middle frame
 # Label(center_frame, text="Search", font=frame_font, bg=cf_bg).place(relx=0.3, rely=0.03)
-search_entry = Entry(center_frame, width=18, font=("Verdana", 12), textvariable=search).place(relx=0.06, rely=0.04)
+search_entry = Entry(center_frame, width=18, font=("Verdana", 12), textvariable=search_str).place(relx=0.06, rely=0.04)
 
 Button(center_frame, text="Search", font=frame_font, width=13, command=search_record).place(relx=0.05, rely=0.1)
 Button(center_frame, text="Add Record", font=frame_font, width=13, command=submit_record).place(relx=0.05, rely=0.2)
