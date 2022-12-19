a76e4ea032cebb2ddd986e3a98e84e0b42fc3eea
ginjardev <arthurshur@gmail.com>
2022-12-19 01:25:04 +0000

    extract song lyrics

---
+++ extract_song_lyrics.py
@@ -0,0 +1,7 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter import scrolledtext
+import requests
+from bs4 import BeautifulSoup
+from googleapiclient.discovery import build
+
