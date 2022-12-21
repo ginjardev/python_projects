2e4d6b61129fb3e0827dc831ef6ff69d36f685e3
ginjardev <arthurshur@gmail.com>
2022-12-21 23:09:48 +0000

    language translator cont

--- language_translator.py
+++ language_translator.py
@@ -1,4 +1,16 @@
-# from tkinter import *
-# from tkinter import messagebox
-# import googletrans
-# from googletrans import Translator
+from tkinter import *
+from tkinter import messagebox
+import googletrans
+from googletrans import Translator
+
+
+app = Tk()
+app.geometry("500x300")
+app.title("Language Translator")
+
+translator_object = Translator()
+
+
+
+def translate_function():
+    pass
