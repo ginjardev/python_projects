4d680fa37970a6f1ee307f5952aeaec30e992216
ginjardev <arthurshur@gmail.com>
2023-01-05 13:06:20 +0000

    yahoo mail firing blank

--- email_sender.py
+++ email_sender.py
@@ -1,4 +1,4 @@
-import smtplib
+import smtplib, ssl
 
 sender='arthurshur@gmail.com'
 receiver = 'arthur_ini@yahoo.com'
@@ -13,6 +13,28 @@ msg = 'Subject: Email from Python script \nLearning about SMTP and the smtplib m
     Kudos to me.'
 smtpserver.sendmail(sender, receiver, msg)
 
-print('Email sent')
+print('Email sent, Gmail')
 
 smtpserver.close()
+
+#testing with yahoo
+
+yusername = 'arthur_ini@yahoo.com'
+yreceiver = 'arthurshur@gmail.com'
+ypassword = 'vbglfpdjpuvhhaus'
+
+context = ssl.create_default_context()
+
+yahoo_smtpserver = smtplib.SMTP('smtp.mail.yahoo.com', 587)
+yahoo_smtpserver.ehlo()
+yahoo_smtpserver.starttls()
+yahoo_smtpserver.ehlo()
+print('server created')
+yahoo_smtpserver.login(yusername, ypassword)
+print('logged in')
+
+ymsg = 'Subject: Yahoo SMTP \nJust saying hi. Bye bye!'
+yahoo_smtpserver.sendmail(yusername, yreceiver, ymsg)
+
+print("Email sent from Yahoo")
+yahoo_smtpserver.close()
